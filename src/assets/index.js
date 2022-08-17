4f5d3d7c932745fb9378a7c457e4abd18fa49477
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2022-08-17 02:15:07 -0600

    Content Done

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,30 +2,25 @@
 let t1= document.getElementById('title')
 let t2= document.getElementById('subtitle')
 let sun = document.getElementById('sun')
-//     let sky = document.getElementById('sky')
-//     let b5 = document.getElementById('b5')
-//     let b4 = document.getElementById('b4')
-//     let b3 = document.getElementById('b3')
-//     let b2 = document.getElementById('b2')
-//     let b1 = document.getElementById('b1')
-
-    
-
-//     t1.style.transform = `translateX(-${window.scrollY/2}px)`
-//     t2.style.transform = `translateX(${window.scrollY/2}px)`
-//     sun.style.transform = `translateY(${window.scrollY/2}px)`
-//     b5.style.transform = `translateY(${(window.scrollY/7)}px)`
-//     b4.style.transform = `translateY(${(window.scrollY/8)}px)`
-//     b3.style.transform = `translateY(${(window.scrollY/9)}px)`
-//     b2.style.transform = `translateY(${(window.scrollY/4)}px)`
-//     b1.style.transform = `translateY(${(window.scrollY/4)}px)`
-//     sky.style.transform = `translateY(${(window.scrollY/4)}px)`
-// })
 
 window.addEventListener('scroll', () => {
     t1.style.transform = `translateX(-${window.scrollY/2}px)`
     t2.style.transform = `translateX(${window.scrollY/2}px)`
-    console.log(scrollY)
+    let sky = document.getElementById('sky')
+    let b5 = document.getElementById('b5')
+    let b4 = document.getElementById('b4')
+    let b3 = document.getElementById('b3')
+    let b2 = document.getElementById('b2')
+    let b1 = document.getElementById('b1')
+
+    if (window.outerWidth > 720) {
+        b5.style.transform = `translateY(${(window.scrollY/7)}px)`
+        b4.style.transform = `translateY(${(window.scrollY/8)}px)`
+        b3.style.transform = `translateY(${(window.scrollY/9)}px)`
+        b2.style.transform = `translateY(${(window.scrollY/4)}px)`
+        b1.style.transform = `translateY(${(window.scrollY/4)}px)`
+        sky.style.transform = `translateY(${(window.scrollY/4)}px)`
+    }
 })
 
 const toggleMenuElement = document.getElementById('toggle-menu')
